ce68bd055b95f0a3e06a5cbdca7cf2b822e009de
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2017-01-24 21:17:10 -0600

    added new files on javascript learning.

---
+++ buttons/index1.js
@@ -0,0 +1,17 @@
+var num = 238;
+var celsius;
+var fahrenheit;
+
+
+function toggleChange(val, id) {
+
+    if (val == "Change to Celsius") {
+        document.getElementById(id).value = "Change to Fahrenheit";
+
+    } else {
+        document.getElementById(id).value = "Change to Celsius";
+    }
+}
+
+    var x = 78;
+    document.getElementById("paragraph").document.write(x);
