01c969c1c89eacc327e85105a2c1d22881efe182
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2017-01-25 22:33:06 -0600

    modified files and folder.

---
+++ Javascript Essentials (Revised)/index.js
@@ -0,0 +1,36 @@
+// A dynamically typed language
+var testString = 'Jesus';
+var testNumber = 42;
+testString = 32;
+
+var  theMeaningOfLife = '42';
+console.log('42' - 2);
+console.log(parseInt(theMeaningOfLife, 10) + 2);
+
+// using type of
+console.log(typeof 'Jesus');
+console.log(typeof 42);
+
+var persons = [{
+    firstName: 'Travis',
+    lastName: 'Tidwell'
+}];
+// use typeof for primative variables
+console.log(typeof persons);
+// use instanceof for instance variables
+console.log(persons instanceof Array);
+console.log(persons instanceof String);
+
+/**
+ *
+ * - Dynamically typed
+ * - Everything is an object... even primitives
+ */
+console.log('Jesus'.length);
+
+/**
+ *  Patterns are just as important as Syntax.
+ *
+ * - http://bit.ly/1de5qfT
+ * - http://bit.ly/1atTe8t
+ */
