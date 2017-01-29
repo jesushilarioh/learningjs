18e15cc7f4f8a371e82a85c3186dcd01c7f30d6a
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2017-01-29 14:20:20 -0600

    added and modified files in javascript events folder.

--- javascript events/index.js
+++ javascript events/index.js
@@ -11,3 +11,28 @@ function add() {
     var sum = one + two;
     addSum.innerHTML = "Your sum is: " + sum;
 }
+
+var orange = document.getElementById("orange");
+var black = document.getElementById("black");
+var red = document.getElementById("red");
+
+
+orange.addEventListener("click", picLink);
+black.addEventListener("click", picLink);
+red.addEventListener("click", picLink);
+
+function picLink() {
+    var allImages = document.querySelectorAll("img");
+
+    for ( var i = 0; i < allImages.length; i++) {
+        allImages[i].className = "hide";
+    }
+
+    var picId = this.attributes["data-grand"].value;
+    var pic = document.getElementById(picId);
+    if (pic.className === "hide") {
+        pic.className = "";
+    } else {
+        pic.className = "hide";
+    }
+}
