8ae8fbef444f72b42e6e41e33f5667b807be1c8e
jesushilariohernandez <jesushilariohernandez@gmail.com>
2017-01-31 21:10:07 -0600

    modified buttons index html and js files.

--- buttons/index1.js
+++ buttons/index1.js
@@ -1,17 +1,18 @@
 var num = 238;
-var celsius;
-var fahrenheit;
+var celsius = 24;
+var fahrenheit = 78;
+var doc = document.getElementById("paragraph");
 
+doc.innerHTML = num;
 
 function toggleChange(val, id) {
 
     if (val == "Change to Celsius") {
         document.getElementById(id).value = "Change to Fahrenheit";
+        doc.innerHTML = celsius + " C";
 
     } else {
         document.getElementById(id).value = "Change to Celsius";
+        doc.innerHTML = fahrenheit + " F";
     }
 }
-
-    var x = 78;
-    document.getElementById("paragraph").document.write(x);
