25537daade87187fca13f949b005b2545c0fe2df
jesushilariohernandez <jesushilariohernandez@gmail.com>
2017-02-07 21:54:53 -0600

    added keypress events.

---
+++ keypress event/index.js
@@ -0,0 +1,6 @@
+var input = document.getElementById('inputBox');
+var keysPressed = document.getElementById('pressLetters');
+
+input.addEventListener("keydown", function() {
+  console.log("hello there!");
+});
