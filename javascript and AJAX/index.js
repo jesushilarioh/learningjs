fc6ccfbf5f223f0580fd18ca033fa309695105de
jesushilariohernandez <jesushilariohernandez@gmail.com>
2017-02-11 20:39:04 -0600

    modified javascript AJAX files.

--- javascript and AJAX/index.js
+++ javascript and AJAX/index.js
@@ -1,10 +1,14 @@
 (function() {
     var httpRequest;
-    document.getElementById("ajaxButton").onclick = function() {
+    var ajaxButton = document.getElementById("ajaxButton");
+    var ajaxTextbox = document.getElementById("ajaxTextbox");
+
+
+    ajaxButton.onclick = function() {
         var word = document.getElementById("ajaxTextbox").value;
         makeRequest('http://api.wordnik.com:80/v4/word.json/'+ word + '/definitions?limit=2&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5');
     };
-    document.getElementById("ajaxTextbox").onkeyup = function() {
+    ajaxTextbox.onkeyup = function() {
         var word = document.getElementById("ajaxTextbox").value;
         makeRequest('http://api.wordnik.com:80/v4/word.json/'+ word + '/definitions?limit=2&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5');
     };
