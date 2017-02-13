fc0779d0ac71380bbf052f01249cd5561627975d
jesushilariohernandez <jesushilariohernandez@gmail.com>
2017-02-12 23:45:57 -0600

    a

--- javascript and AJAX/index.js
+++ javascript and AJAX/index.js
@@ -1,8 +1,11 @@
 (function () {
-    "use strict";
+    //"use strict";
     var httpRequest;
     const ajaxButton = document.getElementById("ajaxButton"),
-        ajaxTextbox = document.getElementById("ajaxTextbox");
+        ajaxTextbox = document.getElementById("ajaxTextbox"),
+        searchedWordHTMLElement = document.getElementById("word"),
+        wordDefinitionHTMLElement = document.getElementById("definitions");
+
 
     ajaxButton.addEventListener("click", getValueFromUser);
     ajaxTextbox.addEventListener("keyup", getValueFromUser);
@@ -25,6 +28,8 @@
     }
 
     function requestStatus() {
+
+
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
             switch (httpRequest.status) {
             case 200:
@@ -51,10 +56,7 @@
     }
 
     function request200() {
-        const searchedWordHTMLElement = document.getElementById("word"),
-            wordDefinitionHTMLElement = document.getElementById("definitions"),
-            JSONParse = JSON.parse(httpRequest.responseText);
-
+        const JSONParse = JSON.parse(httpRequest.responseText);
         if (typeof (JSONParse[0]) == 'undefined') {
             innerBlankHTML(searchedWordHTMLElement, false);
             searchedWordHTMLElement.innerHTML = "I'm sorry, " + "<strong id='wrongWord'>" + ajaxTextbox.value + "</strong>" + " is not a word...";
@@ -69,24 +71,17 @@
     }
 
     function request400() {
-        const searchedWordHTMLElement = document.getElementById("word"),
-            wordDefinitionHTMLElement = document.getElementById("definitions");
-
         innerBlankHTML(searchedWordHTMLElement, true);
         innerBlankHTML(wordDefinitionHTMLElement, true);
 
     }
 
     function request404() {
-        const searchedWordHTMLElement = document.getElementById("word"),
-            wordDefinitionHTMLElement = document.getElementById("definitions");
-
         innerBlankHTML(searchedWordHTMLElement, true);
         innerBlankHTML(wordDefinitionHTMLElement, true);
     }
 
     function requestOther() {
-        const wordDefinitionHTMLElement = document.getElementById("definitions");
         wordDefinitionHTMLElement.innerHTML = "We're sorry, your request could not be processed at this time.";
     }
 })();
