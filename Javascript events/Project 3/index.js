bbdbcaac0722e34000f5c00680f296d0917e67de
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2017-02-18 13:35:42 -0600

    added new event folder and files... love it!!

--- Javascript events/Project 3/index.js
+++ Javascript events/Project 3/index.js
@@ -1,3 +1,10 @@
 (function() {
+    const userInput = document.getElementById("userInput"),
+          mainLink = document.getElementById("mainLink");
 
+    mainLink.addEventListener('click', function() {
+
+        console.log(mainLink.textContent);
+        userInput.value = mainLink.textContent;
+    })
 })();
