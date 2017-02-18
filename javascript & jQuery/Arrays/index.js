d6894a373384f4f7f9b17ab5add4765f49d32d82
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2017-02-18 13:09:55 -0600

    modified javascript & jQuery files.

--- javascript & jQuery/Arrays/index.js
+++ javascript & jQuery/Arrays/index.js
@@ -1,21 +1,29 @@
 (function() {
-    // Below are two ways to create an array
-    // array literal:
+    // Creating An Array
+    // Array Literal:
     let colors;
     colors = ['white',
-        'black',
-        'custom'
+              'black',
+              'custom'
     ];
 
     let el = document.getElementById("colors");
     el.textContent = colors[0];
 
 
-    // array constructor:
+    // Array Constructor:
     let colors2 = new Array('grand red',
-        'green seven',
-        'seven orange');
+                            'green seven',
+                            'seven orange');
 
     var el2 = document.getElementById("colors2");
     el2.textContent = colors2[0];
+
+
+    // Accessing & Changing Values In An Array
+    // Update the third item in the colors array
+    colors[2] = 'baige';
+
+    // Replace el with third item from the array
+    el.textContent = colors[2];
 })();
