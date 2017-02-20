2fd18fc4e3ea5b83f78361028aef4acb8ee0ea38
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2017-02-20 16:04:57 -0600

    modified index.js

--- Javascript events/Project 3/index.js
+++ Javascript events/Project 3/index.js
@@ -1,12 +1,13 @@
 (function() {
-    var userInput = document.getElementById("userInput"),
+    // Constant Variables
+    const userInput = document.getElementById("userInput"),
           mainLink = document.getElementById("mainLink"),
           section = document.getElementById("section");
-    let listOfWords = ['brand', 'grand', 'staple', 'topple', 'johnny', 'utah'];
 
-    let link = [];
+    // Variables
+    var listOfWords = ['brand', 'grand', 'staple', 'topple', 'johnny', 'utah'];
 
-    // Create a series of links
+    // Create a link for each word in listOfWords array
     for (i = 0; i < listOfWords.length; i++) {
         let newLink = document.createElement("A");
         let text = document.createTextNode(listOfWords[i]);
@@ -14,25 +15,18 @@
         newLink.setAttribute('href', '#');
         newLink.setAttribute('value', listOfWords[i]);
         newLink.appendChild(text);
-
     }
 
-    let links = section.querySelectorAll("a");
-    console.log(links);
-    console.log(links[3].text);
+    // Assign all links to links variables
+    var links = section.querySelectorAll("a");
 
+    // Add an event listenr to each link
+    for (var i = 0; i < listOfWords.length; i++) {
+        links[i].addEventListener("click", function() {
 
-        links[1].addEventListener("click", function() {
-            console.log("hello");
-            userInput.value = links[1].text;
-            console.log(userInput.value);
+            // When click the value will be replaced in input box
+            userInput.value = this.textContent;
         });
+    }
 
-
-        // userInput.value = links[i].textContent;
-
-    /*mainLink.addEventListener('click', function() {
-        console.log(mainLink.textContent);
-        userInput.value = mainLink.textContent;
-    })*/
 })();
