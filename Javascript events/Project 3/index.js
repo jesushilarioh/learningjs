472636704e7cbee9cd377a37b19a3e98cb2e4c98
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2017-02-19 20:13:31 -0600

    added new info.

--- Javascript events/Project 3/index.js
+++ Javascript events/Project 3/index.js
@@ -1,6 +1,22 @@
 (function() {
     const userInput = document.getElementById("userInput"),
-          mainLink = document.getElementById("mainLink");
+          mainLink = document.getElementById("mainLink"),
+          section = document.getElementById("section");
+    let listOfWords = ['brand', 'grand', 'staple', 'topple', 'johnny', 'utah'];
+
+
+    // Create a series of links
+    for (i = 0; i < listOfWords.length; i++) {
+        let newLink = document.createElement("A");
+        let text = document.createTextNode(listOfWords[i]);
+        section.appendChild(document.createElement("BR"));
+        section.appendChild(newLink);
+        newLink.setAttribute('href', '#');
+        newLink.setAttribute('value', listOfWords[i]);
+        newLink.appendChild(text);
+
+    }
+
 
     mainLink.addEventListener('click', function() {
         console.log(mainLink.textContent);
