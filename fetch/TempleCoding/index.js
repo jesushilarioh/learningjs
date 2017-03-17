e756f64d758de858ebd1de62abd46040fccba176
Jesus Hilario <jesushilariohernandez@gmail.com>
2017-03-16 23:42:26 -0500

    modified.

--- fetch/TempleCoding/index.js
+++ fetch/TempleCoding/index.js
@@ -1,15 +1,24 @@
-(function () {
+(function() {
     'use strict';
 
-    let promise = fetch('https://api.github.com/users/jesushilarioh');
-    // work with success method of a promise
-    promise.then(response => {
-        console.log(response);
-        return response.json();
-    }).then(json => {
-        document.body.innerHTML = json.name;
-    }).catch(reason => {
-        console.log("Catch: ", reason);
-    });
+    var myHeaders = new Headers();
+    myHeaders.append('Api-User-Agent', 'Example/1.0')
+    var myInit = {
+        method: 'GET',
+        headers: myHeaders,
+        mode: 'no-cors',
+        cache: 'default'
+    };
+
+    fetch('https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=grand', myInit)
+        // work with success method of a promise
+        .then(response => {
+            console.log(response);
+            return response.text()
+        }).then(data => {
+            document.body.textContent = data;
+        }).catch(reason => {
+            console.log("Catch: ", reason);
+        });
 
 })();
